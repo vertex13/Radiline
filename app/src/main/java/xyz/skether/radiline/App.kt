dce94b672601728567c80f3cd5c73ecd0474a02a
Aleksandr Pavlov <vertex55reg@gmail.com>
2019-02-28 18:16:11 +0600

    add the leakcanary library

--- app/src/main/java/xyz/skether/radiline/App.kt
+++ app/src/main/java/xyz/skether/radiline/App.kt
@@ -1,12 +1,18 @@
 package xyz.skether.radiline
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         logInfo("App is created")
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+        LeakCanary.install(this)
     }
 
 }
